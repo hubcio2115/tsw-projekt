af0a9f48f39c97e6fa6db6cbdf1afb64cbf2371e
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-07 11:52:09 +0100

    formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
-const animate = require("tailwindcss-animate")
+const animate = require("tailwindcss-animate");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  
+
   theme: {
     container: {
       center: true,
@@ -64,10 +64,10 @@ module.exports = {
         },
         "collapsible-down": {
           from: { height: 0 },
-          to: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: "var(--radix-collapsible-content-height)" },
         },
         "collapsible-up": {
-          from: { height: 'var(--radix-collapsible-content-height)' },
+          from: { height: "var(--radix-collapsible-content-height)" },
           to: { height: 0 },
         },
       },
@@ -80,4 +80,4 @@ module.exports = {
     },
   },
   plugins: [animate],
-}
+};
