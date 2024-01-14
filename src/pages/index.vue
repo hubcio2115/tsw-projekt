76113c0f1a950f814c41762bcc84d176ca23a946
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-14 11:59:40 +0100

    added to the main view preview of authentication

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,27 @@
 <script setup>
-import { useState } from "#imports";
-
 import { Button } from "~/components/ui/button";
 
-const counter = useState("counter", () => 0);
+const { signOut, session, user, status, signIn } = useAuth();
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col items-center justify-center">
     <h1 class="text-2xl font-bold">Hello from Nuxt!</h1>
 
-    <Button @click="counter++">Counter: {{ counter }} </Button>
+    <Button @click="signOut({ callbackUrl: '/' })"> Log out </Button>
+
+    <Button @click="signIn('google')"> Log in </Button>
+
+    <p>
+      {{ session?.user?.name }}
+    </p>
+
+    <p>
+      {{ user?.name }}
+    </p>
+
+    <p>
+      {{ status }}
+    </p>
   </div>
 </template>
