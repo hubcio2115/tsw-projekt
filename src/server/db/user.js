456d31dfc745b6bb86fd6d154df4b6f3d61510a8
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-17 10:26:42 +0100

    updated users query funcitons

--- src/server/db/user.js
+++ src/server/db/user.js
@@ -29,11 +29,33 @@ export async function getUserByUsername(username) {
 /**
  * @param {import("~/lib/validators/user").AuthUser} newUser
  */
-export async function createUser({ username, password, email }) {
+export async function createUser({ name, password, email }) {
   const hashedPassword = await hash(password);
 
   await neo4jSession.run(
-    "CREATE (u:User {id: $id, email: $email, password: $password, username: $username})",
-    { id: crypto.randomUUID(), username, password: hashedPassword, email },
+    "MERGE (u:User {id: $id, email: $email, password: $password, username: $name})",
+    { id: crypto.randomUUID(), name, password: hashedPassword, email },
+  );
+}
+
+/**
+ * @param {import("~/lib/validators/user").User["id"]} userId
+ * @param {import("~/lib/validators/user").User["id"]} targetUserId
+ */
+export async function followUser(userId, targetUserId) {
+  return await neo4jSession.run(
+    "MATCH (u1:User {id: $userId}) MATCH (u2: User {id: $targetUserId}) MERGE (u1)-[:FOLLOWS]->(u2)",
+    { userId, targetUserId },
+  );
+}
+
+/**
+ * @param {import("~/lib/validators/user").User["id"]} userId
+ * @param {import("~/lib/validators/user").User["id"]} targetUserId
+ */
+export async function unfollowUser(userId, targetUserId) {
+  return await neo4jSession.run(
+    "MATCH (u1:User {id: $userId})-[f:FOLLOWS]->(u2: User {id: $targetUserId}) DELETE f",
+    { userId, targetUserId },
   );
 }
