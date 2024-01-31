b1d7fc505e487d4d619b0756225963fd8e8bea3c
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-31 10:05:32 +0100

    updated layout to vite

--- apps/web/src/layouts/MainLayout.vue
+++ apps/web/src/layouts/MainLayout.vue
@@ -1,19 +1,29 @@
 <script setup>
+import { useMutation, useQuery } from "@tanstack/vue-query";
 import { VueQueryDevtools } from "@tanstack/vue-query-devtools";
+import axios from "axios";
 import { Home, User } from "lucide-vue-next";
 import { LogOut, MoreHorizontal } from "lucide-vue-next";
 import { storeToRefs } from "pinia";
+import io from "socket.io-client";
+import { onMounted, onUnmounted } from "vue";
 import { RouterView, useRoute, useRouter } from "vue-router";
+import { z } from "zod";
 
+import PostTextArea from "~/components/PostTextArea.vue";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
+import { env } from "~/env.mjs";
 import { cn, getInitials } from "~/lib/utils";
+import { postSchema } from "~/lib/validators/post";
+import { userSchema } from "~/lib/validators/user";
 import { useAuthStore } from "~/store/authStore";
 
 const router = useRouter();
@@ -22,96 +32,204 @@ const route = useRoute();
 const store = useAuthStore();
 const { user } = storeToRefs(store);
 
-function signOut() {}
+/** @type {ReturnType<typeof $ref<import('socket.io-client').Socket | null>>} */
+let socket = $ref(null);
+
+onMounted(() => {
+  socket = io(`${env.VITE_API_BASE_URL}`);
+
+  socket.on("connection", () => {
+    console.log("Connected to socket.");
+  });
+
+  /** @param {{ sender: string; target: string; senderUsername: string}} followEvent */
+  socket.on("followed", (followEvent) => {
+    if (followEvent.target === user.value?.id) {
+      alert(`${followEvent.senderUsername} followed you!`);
+    }
+  });
+});
+
+onUnmounted(() => {
+  socket?.disconnect();
+});
+
+const { data: users, isPending: fetchingUsers } = useQuery({
+  queryKey: ["users"],
+  queryFn: async () => {
+    const res = await axios.get(`${env.VITE_API_BASE_URL}/api/users`, {
+      withCredentials: true,
+    });
+
+    if (res.status >= 200 || res.status <= 299) {
+      const users = z.array(userSchema).parse(res.data);
+
+      return users;
+    }
+
+    return [];
+  },
+});
+
+async function signOut() {
+  const res = await axios.get(`${env.VITE_API_BASE_URL}/api/auth/logout`, {
+    withCredentials: true,
+  });
+
+  if (res.status >= 200 || res.status <= 299) {
+    store.unsetUser();
+    await router.push("/auth");
+  }
+}
+
+const postMutation =
+  /** @type {typeof useMutation<import("~/lib/validators/post").Post | null, Error, string>} */ (
+    useMutation
+  )({
+    mutationKey: ["post", "create"],
+    mutationFn: async (content) => {
+      const res = await axios.post(
+        `${env.VITE_API_BASE_URL}/api/posts`,
+        { content },
+        { withCredentials: true },
+      );
+
+      const isSucessful = res.status >= 200 || res.status <= 299;
+      if (!isSucessful) {
+        throw new Error(res.statusText);
+      }
+
+      const data = postSchema.parse(await res.data);
+
+      return data;
+    },
+
+    onSuccess: (data, [userId]) => {
+      router.push(`/${userId}/posts/${data?.id}`);
+    },
+  });
+
+/**
+ * @param {string} content
+ */
+function onSubmit(content) {
+  postMutation.mutate(content);
+}
 
 const initials = $computed(() =>
   getInitials(user.value?.firstName, user.value?.lastName),
 );
+
+/**
+ * @param {string} userId
+ */
+function goToUserProfile(userId) {
+  router.push(`/${userId}`);
+}
 </script>
 
 <template>
-  <div>
-    <div class="mx-auto grid min-h-screen max-w-5xl grid-cols-4 gap-4">
-      <nav class="flex h-full flex-col gap-2 py-4">
-        <Button
-          variant="ghost"
-          class="flex h-12 w-12 rounded-full text-3xl"
-          @click="router.push('/')"
-        >
-          𝕐
-        </Button>
+  <div class="mx-auto grid min-h-screen max-w-5xl grid-cols-4 gap-4">
+    <nav class="flex h-full flex-col gap-2 py-4">
+      <Button
+        variant="ghost"
+        class="flex h-12 w-12 rounded-full text-3xl"
+        @click="router.push('/home')"
+      >
+        𝕐
+      </Button>
 
-        <Button
-          variant="ghost"
-          :class="
-            cn(
-              'max-w-min items-start justify-start rounded-full text-2xl',
-              route.fullPath === '/' ? 'font-bold' : '',
-            )
-          "
-          @click="router.push('/')"
-        >
-          <Home :size="24" />
-          Home
-        </Button>
+      <Button
+        variant="ghost"
+        :class="
+          cn(
+            'max-w-min items-start justify-start rounded-full text-2xl',
+            route.fullPath === '/home' ? 'font-bold' : '',
+          )
+        "
+        @click="router.push('/')"
+      >
+        <Home :size="24" />
+        Home
+      </Button>
 
-        <Button
-          variant="ghost"
-          :class="
-            cn(
-              'max-w-min items-start justify-start rounded-full text-2xl',
-              route.fullPath === '/profile' ? 'font-bold' : '',
-            )
-          "
-          @click="router.push('/profile')"
-        >
-          <User :size="24" />
-          Profile
-        </Button>
+      <Button
+        variant="ghost"
+        :class="
+          cn(
+            'max-w-min items-start justify-start rounded-full text-2xl',
+            route.fullPath === `/${user?.id}` ? 'font-bold' : '',
+          )
+        "
+        @click="goToUserProfile(user?.id)"
+      >
+        <User :size="24" />
+        Profile
+      </Button>
 
-        <Button
-          variant="ghost"
-          class="justify-center rounded-full bg-blue-400 p-6 text-lg font-bold text-white hover:bg-blue-500 hover:text-white focus-visible:outline-blue-400"
+      <Dialog>
+        <DialogTrigger
+          class="justify-center rounded-full bg-blue-400 p-3 text-lg font-bold text-white hover:bg-blue-500 hover:text-white focus-visible:outline-blue-400"
         >
           Post
-        </Button>
+        </DialogTrigger>
 
-        <DropdownMenu>
-          <DropdownMenuTrigger as-child>
-            <Button
-              variant="ghost"
-              class="mb-0 mt-auto justify-start rounded-full px-4 py-8"
-            >
-              <Avatar class="h-10 w-10">
-                <AvatarImage alt="profile-picture" />
-                <AvatarFallback>{{ initials }}</AvatarFallback>
-              </Avatar>
+        <DialogContent class="top-[15%]">
+          <PostTextArea
+            placeholder="What is happening?"
+            @on-submit="onSubmit"
+          />
+        </DialogContent>
+      </Dialog>
 
-              <div>
-                <p class="font-bold">
-                  {{ user?.firstName }} {{ user?.lastName }}
-                </p>
-                <p class="text-left">@{{ user?.username }}</p>
-              </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <Button
+            variant="ghost"
+            class="mb-0 mt-auto justify-start rounded-full px-4 py-8"
+          >
+            <Avatar class="h-10 w-10">
+              <AvatarImage alt="profile-picture" />
+              <AvatarFallback>{{ initials }}</AvatarFallback>
+            </Avatar>
 
-              <MoreHorizontal class="ml-auto mr-0" />
-            </Button>
-          </DropdownMenuTrigger>
+            <div>
+              <p class="font-bold">
+                {{ user?.firstName }} {{ user?.lastName }}
+              </p>
+              <p class="text-left">@{{ user?.username }}</p>
+            </div>
 
-          <DropdownMenuContent class="w-56 rounded-xl">
-            <DropdownMenuItem
-              class="cursor-pointer rounded-xl text-red-500 hover:text-red-600 focus:text-red-600"
-              @click="signOut()"
-            >
-              Log out
-              <LogOut class="ml-auto mr-0" :size="16" />
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </nav>
+            <MoreHorizontal class="ml-auto mr-0" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <RouterView />
-    </div>
-    <VueQueryDevtools />
+        <DropdownMenuContent class="w-56 rounded-xl">
+          <DropdownMenuItem
+            class="cursor-pointer rounded-xl text-red-500 hover:text-red-600 focus:text-red-600"
+            @click="signOut()"
+          >
+            Log out
+            <LogOut class="ml-auto mr-0" :size="16" />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
+
+    <RouterView :key="route.fullPath" />
+
+    <ul class="flex flex-col items-center pt-4">
+      <Spinner v-if="fetchingUsers" />
+
+      <template v-else-if="users">
+        <li v-for="userProfile in users" :key="userProfile?.id">
+          <span
+            class="cursor-pointer hover:underline"
+            @click="goToUserProfile(userProfile?.id)"
+            >@{{ userProfile?.username }}</span
+          >
+        </li>
+      </template>
+    </ul>
   </div>
 </template>
