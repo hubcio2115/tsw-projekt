e79638fb2e8c6b9f3ce0311c370baf324e6eea6a
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-31 10:03:04 +0100

    deleted express.d.ts since it didn't seem to do anything

--- apps/api/src/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { type User as TUser } from "~/validators/user";
-
-declare module "express-serve-static-core" {
-  interface User extends TUser { }
-
-  interface Request {
-    user?: User;
-  }
-}
