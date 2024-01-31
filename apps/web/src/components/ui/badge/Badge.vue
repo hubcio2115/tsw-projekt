06acddcce23f95d3604dca08d07c0e9b6c32b35e
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-31 14:12:36 +0100

    added badge component

---
+++ apps/web/src/components/ui/badge/Badge.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { cn } from "~/lib/utils";
+
+import { badgeVariants } from ".";
+
+defineProps({
+  variant: { type: null, required: false, default: "default" },
+});
+</script>
+
+<template>
+  <div :class="cn(badgeVariants({ variant }), $attrs.class ?? '')">
+    <slot />
+  </div>
+</template>
