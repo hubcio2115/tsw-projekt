48367f7ec58cf05508d06c1dd64ea3a4b53b4206
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-31 14:12:30 +0100

    added namespaces for follow and post websockets

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -12,19 +12,18 @@ import { posts } from "./routers/posts.js";
 import { root } from "./routers/root.js";
 import { users } from "./routers/users.js";
 import { readFileSync } from "node:fs";
+import { getUserFollowers } from "./db/user.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use((_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://localhost:5173");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, set-cookie");
-  next();
-});
+app.use(
+  cors({
+    origin: "https://localhost:5173",
+    credentials: true,
+  }),
+);
 
 const memoryStore = new session.MemoryStore();
 
@@ -36,7 +35,6 @@ app.use(
     saveUninitialized: false,
     store: memoryStore,
     cookie: {
-      httpOnly: false,
       secure: true,
     },
   }),
@@ -66,8 +64,10 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("Connected ", socket.id);
+const followNamespace = io.of("/follow");
+
+followNamespace.on("connection", (socket) => {
+  console.log("Connected to /follow ", socket.id);
 
   socket.on(
     "follow",
@@ -75,7 +75,31 @@ io.on("connection", (socket) => {
       message,
     ) => {
       console.debug(message);
-      io.emit("followed", message);
+      followNamespace.emit("followed", message);
+    },
+  );
+
+  socket.on("disconnect", () => {
+    console.log("Disconnect ", socket.id);
+  });
+});
+
+
+const postNamespace = io.of("/post");
+
+postNamespace.on("connection", (socket) => {
+  console.log("Connected to /post ", socket.id);
+
+  socket.on(
+    "post",
+    /** @param {string} userId */ async (
+      userId,
+    ) => {
+      const followers = await getUserFollowers(userId);
+
+      console.log(followers);
+      
+      postNamespace.emit("posted", followers);
     },
   );
 
