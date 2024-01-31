1ebb4f9c175c0a273a53e20d2f82a216090d6a5e
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-31 14:15:12 +0100

    added refetch badge on new posts

--- apps/web/src/pages/HomePage.vue
+++ apps/web/src/pages/HomePage.vue
@@ -1,19 +1,54 @@
 <script setup>
 import { useQuery } from "@tanstack/vue-query";
 import axios from "axios";
-import { Loader2 } from "lucide-vue-next";
+import { Loader2, RotateCw } from "lucide-vue-next";
+import { io } from "socket.io-client";
+import { onMounted, onUnmounted } from "vue";
 import { z } from "zod";
 
 import PostComponent from "~/components/PostComponent.vue";
 import Spinner from "~/components/ui/Spinner.vue";
+import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
 import { env } from "~/env.mjs";
 import { postSchema } from "~/lib/validators/post.js";
 import { userSchema } from "~/lib/validators/user";
+import { useAuthStore } from "~/store/authStore";
+
+const store = useAuthStore();
+
+/** @type {ReturnType<typeof $ref<import('socket.io-client').Socket | null>>} */
+let postSocket = $ref(null);
+
+let areNewPostsAvailable = $ref(false);
+
+onMounted(() => {
+  postSocket = io(`${env.VITE_API_BASE_URL}/post`);
+
+  postSocket.on("connection", () => {
+    console.log("Connected to /post.");
+  });
+
+  postSocket.on(
+    "posted",
+    /** @param {string[]} userIds */ (userIds) => {
+      const isUserInFollowers = userIds.some((id) => id === store.user?.id);
+
+      if (isUserInFollowers) {
+        areNewPostsAvailable = true;
+      }
+    },
+  );
+});
+
+onUnmounted(() => {
+  postSocket?.disconnect();
+});
 
 const {
   data: posts,
   isPending,
+  isFetching,
   refetch,
 } = useQuery({
   queryKey: ["home", "posts"],
@@ -38,6 +73,11 @@ const {
     return null;
   },
 });
+
+function handleOnBadgeClick() {
+  refetch();
+  areNewPostsAvailable = false;
+}
 </script>
 
 <template>
@@ -49,7 +89,15 @@ const {
     </Head>
 
     <div class="flex h-full w-full flex-col">
-      <Spinner v-if="isPending" />
+      <Badge
+        v-if="areNewPostsAvailable"
+        class="absolute left-[calc(50%-80px)] top-2 mx-auto w-40 cursor-pointer fade-in fade-out"
+        @click="handleOnBadgeClick"
+      >
+        New post available <RotateCw class="ml-2" :size="14" />
+      </Badge>
+
+      <Spinner v-if="isPending || isFetching" />
 
       <div
         v-else-if="!posts"
@@ -61,7 +109,9 @@ const {
           But if you don't want to do that try refetching in couple of minutes.
         </p>
 
-        <Button variant="ghost" @click="refetch"><Loader2 /></Button>
+        <Button variant="ghost" @click="refetch">
+          <Loader2 />
+        </Button>
       </div>
 
       <div
