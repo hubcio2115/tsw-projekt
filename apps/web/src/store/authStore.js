7a0848b304414c7dd81deabdc8c8af1f8870c82b
hubcio2115 <kowalski.hubercik1@gmail.com>
2024-01-31 14:13:56 +0100

    fixed authStore bug with not updating the user state

--- apps/web/src/store/authStore.js
+++ apps/web/src/store/authStore.js
@@ -1,31 +1,33 @@
+import { useStorage } from "@vueuse/core";
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
 // You can name the return value of `defineStore()` anything you want,
 // but it's best to use the name of the store and surround it with `use`
 // and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
 // the first argument is a unique id of the store across your application
 export const useAuthStore = defineStore("auth", () => {
-  const userFromStorage = localStorage.getItem("user");
+  const userFromStorage = useStorage("user", localStorage.getItem("user"));
 
-  let user = $ref(
-    userFromStorage !== null
+  const user = ref(
+    userFromStorage.value !== null
       ? /** @type {import('~/lib/validators/user').User}  */ (
-          JSON.parse(userFromStorage)
+          JSON.parse(userFromStorage.value)
         )
       : null,
   );
 
   /** @param {import("~/lib/validators/user").User} newUser */
   function setUser(newUser) {
-    user = newUser;
+    user.value = newUser;
 
-    localStorage.setItem("user", JSON.stringify(newUser));
+    userFromStorage.value = JSON.stringify(newUser);
   }
 
   function unsetUser() {
-    user = null;
+    user.value = null;
 
-    localStorage.removeItem("user");
+    userFromStorage.value = null;
   }
 
   return { user, setUser, unsetUser };
